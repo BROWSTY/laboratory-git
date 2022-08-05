3cb260b5077295c023be7ee4b01d5143ce7a190c
Patricio-Mendoza <patricio.mendoza@yachaytech.edu.ec>
2022-08-05 12:28:49 -0500

    change

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const $btnSignIn= document.querySelector('.sign-in-btn'),
       $btnSignUp = document.querySelector('.sign-up-btn'),  
+      $btnPass = document.querySelector('.pass-btn'),
       $signUp = document.querySelector('.sign-up'),
       $signIn  = document.querySelector('.sign-in');
+      $passWord  = document.querySelector('.password');
 
 document.addEventListener('click', e => {
     if (e.target === $btnSignIn || e.target === $btnSignUp) {
         $signIn.classList.toggle('active');
-        $signUp.classList.toggle('active')
+        $signUp.classList.toggle('active');
+        $passWord.classList.toggle('active')
     }
 });
